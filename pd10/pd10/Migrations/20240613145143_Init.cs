4e71fd3e47bae5594ac3c4792a9cb7366b9efdb0
aniaszpala14 <s27542@pjwstk.edu.pl>
2024-06-13 18:14:06 +0200

    files

---
+++ pd10/pd10/Migrations/20240613145143_Init.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace pd10.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    IdDoctor = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.IdDoctor);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicaments",
+                columns: table => new
+                {
+                    IdMedicament = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicaments", x => x.IdMedicament);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    IdPatient = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.IdPatient);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prescription",
+                columns: table => new
+                {
+                    IdPrescription = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IdPatient = table.Column<int>(type: "int", nullable: false),
+                    IdDoctor = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescription", x => x.IdPrescription);
+                    table.ForeignKey(
+                        name: "FK_Prescription_Doctors_IdDoctor",
+                        column: x => x.IdDoctor,
+                        principalTable: "Doctors",
+                        principalColumn: "IdDoctor",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Prescription_Patients_IdPatient",
+                        column: x => x.IdPatient,
+                        principalTable: "Patients",
+                        principalColumn: "IdPatient",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prescription_Medicament",
+                columns: table => new
+                {
+                    IdPrescription = table.Column<int>(type: "int", nullable: false),
+                    IdMedicament = table.Column<int>(type: "int", nullable: false),
+                    Dose = table.Column<int>(type: "int", nullable: false),
+                    Details = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prescription_Medicament", x => new { x.IdPrescription, x.IdMedicament });
+                    table.ForeignKey(
+                        name: "FK_Prescription_Medicament_Medicaments_IdMedicament",
+                        column: x => x.IdMedicament,
+                        principalTable: "Medicaments",
+                        principalColumn: "IdMedicament",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Prescription_Medicament_Prescription_IdPrescription",
+                        column: x => x.IdPrescription,
+                        principalTable: "Prescription",
+                        principalColumn: "IdPrescription",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Doctors",
+                columns: new[] { "IdDoctor", "Email", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "j@kow", "Jan", "Kowalski" },
+                    { 2, "a@now", "Anna", "Nowak" },
+                    { 3, "j@dr", "Jan", "Drabina" },
+                    { 4, "a@dr", "Anna", "Drabina" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicaments",
+                columns: new[] { "IdMedicament", "Description", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "bol glowy", "Apap", "tabletki" },
+                    { 2, "bol", "Ibuprofen", "tabletki" },
+                    { 3, "suplement", "Kwas omega", "tabletki" },
+                    { 4, "suplement", "Witamina C", "tabletki" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Patients",
+                columns: new[] { "IdPatient", "Birthdate", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1990, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Jan", "Kowalski" },
+                    { 2, new DateTime(1991, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Anna", "Nowak" },
+                    { 3, new DateTime(1992, 3, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Jan", "Drabina" },
+                    { 4, new DateTime(1993, 4, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Anna", "Drabina" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescription",
+                columns: new[] { "IdPrescription", "Date", "DueDate", "IdDoctor", "IdPatient" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2021, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2021, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), 1, 1 },
+                    { 2, new DateTime(2021, 2, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2021, 2, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), 2, 2 },
+                    { 3, new DateTime(2021, 3, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(2021, 3, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), 3, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prescription_Medicament",
+                columns: new[] { "IdMedicament", "IdPrescription", "Details", "Dose" },
+                values: new object[,]
+                {
+                    { 1, 1, "codziennie", 2 },
+                    { 2, 2, "codziennie", 3 },
+                    { 3, 3, "codziennie", 1 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescription_IdDoctor",
+                table: "Prescription",
+                column: "IdDoctor");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescription_IdPatient",
+                table: "Prescription",
+                column: "IdPatient");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prescription_Medicament_IdMedicament",
+                table: "Prescription_Medicament",
+                column: "IdMedicament");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prescription_Medicament");
+
+            migrationBuilder.DropTable(
+                name: "Medicaments");
+
+            migrationBuilder.DropTable(
+                name: "Prescription");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+        }
+    }
+}
